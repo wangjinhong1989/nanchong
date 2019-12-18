a7415ad1c64db76f5421dd623b9e1305dbfa6eea
王金宏 <2709989114@qq.com>
2019-12-18 13:59:02 +0800

    1

---
+++ public/assets/js/frontend/contact.js
@@ -0,0 +1,11 @@
+define(['jquery', 'bootstrap', 'frontend', 'form', 'template'], function ($, undefined, Frontend, Form, Template) {
+    var Controller = {
+        index: function () {
+
+        },
+        detail: function () {
+
+        }
+    };
+    return Controller;
+});
